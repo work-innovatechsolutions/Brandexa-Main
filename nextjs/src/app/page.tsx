172e0cacf505ea3cd789afa30d7f87722b8a1cdd
souvikgon377 <souvikgon377@gmail.com>
2026-07-09 00:53:28 +0530

    added all things with works

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -597,102 +597,68 @@ export default function Home() {
 </div>
 </div>
 </div>
-<div className="elementor-element elementor-element-9307b97 e-flex e-con-boxed e-con e-parent" data-e-type="container" data-element_type="container" data-id="9307b97">
-<div className="e-con-inner">
-<div className="elementor-element elementor-element-ff82b5d e-con-full e-flex e-con e-child" data-e-type="container" data-element_type="container" data-id="ff82b5d">
-<div className="elementor-element elementor-element-89a12e3 e-con-full e-flex e-con e-child" data-e-type="container" data-element_type="container" data-id="89a12e3">
-<div className="elementor-element elementor-element-656d3c2 section-title at-heading-animation at-animation-heading-none elementor-invisible elementor-widget elementor-widget-heading" data-e-type="widget" data-element_type="widget" data-id="656d3c2" data-settings="{&quot;_animation&quot;:&quot;fadeInUp&quot;,&quot;ekit_we_effect_on&quot;:&quot;none&quot;}" data-widget_type="heading.default">
-<div className="elementor-widget-container">
-<h2 className="elementor-heading-title elementor-size-default">JOIN AGENCY</h2> </div>
+<section className="brandexa-social-section" aria-labelledby="brandexa-social-title">
+<div className="brandexa-social-inner">
+<div className="brandexa-social-top">
+<div className="brandexa-social-heading">
+<p className="brandexa-section-kicker" id="brandexa-social-title">JOIN AGENCY</p>
+<h2>Join our <span>agency</span> of creative innovators</h2>
 </div>
-<div className="elementor-element elementor-element-6a32270 at-heading-animation at-animation-heading-style-3 elementor-widget elementor-widget-heading" data-e-type="widget" data-element_type="widget" data-id="6a32270" data-settings="{&quot;ekit_we_effect_on&quot;:&quot;none&quot;}" data-widget_type="heading.default">
-<div className="elementor-widget-container">
-<h2 className="elementor-heading-title elementor-size-default">Join our <span>agency</span> of creative innovators</h2> </div>
+<p className="brandexa-social-copy">Join our creative community to collaborate, innovate, and thrive together. We welcome passionate individuals eager to make.</p>
 </div>
-</div>
-<div className="elementor-element elementor-element-30a7a07 e-con-full e-flex e-con e-child" data-e-type="container" data-element_type="container" data-id="30a7a07">
-<div className="elementor-element elementor-element-1d2c6c8 elementor-invisible elementor-widget elementor-widget-text-editor" data-e-type="widget" data-element_type="widget" data-id="1d2c6c8" data-settings="{&quot;_animation&quot;:&quot;fadeInUp&quot;,&quot;_animation_delay&quot;:100,&quot;ekit_we_effect_on&quot;:&quot;none&quot;}" data-widget_type="text-editor.default">
-<div className="elementor-widget-container">
-<p>Join our creative community to collaborate, innovate, and thrive together We welcome passionate individuals eager to make.</p> </div>
-</div>
-</div>
-</div>
-<div className="elementor-element elementor-element-1fdf852 e-con-full e-flex e-con e-child" data-e-type="container" data-element_type="container" data-id="1fdf852">
-<div className="elementor-element elementor-element-1efc536 elementor-widget__width-initial agency-social-item ekit-equal-height-disable elementor-invisible elementor-widget elementor-widget-elementskit-icon-box" data-e-type="widget" data-element_type="widget" data-id="1efc536" data-settings="{&quot;_animation&quot;:&quot;fadeInUp&quot;,&quot;ekit_we_effect_on&quot;:&quot;none&quot;}" data-widget_type="elementskit-icon-box.default">
-<div className="elementor-widget-container">
-<div className="ekit-wid-con"> {/* link opening */}
-{/* end link opening */}
-<div className="elementskit-infobox text-left text-left icon-top-align elementor-animation-">
-<div className="elementskit-box-header elementor-animation-">
-<div className="elementskit-info-box-icon">
-<svg aria-hidden="true" className="elementkit-infobox-icon e-font-icon-svg e-fab-x-twitter" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"></path></svg>
-</div>
-</div>
-<div className="box-body">
-<h3 className="elementskit-info-box-title">
-                    Follow Us On Twitter                </h3>
-<p>@artistic_official</p>
-<div className="box-footer disable_hover_button">
-<div className="btn-wraper">
-<a className="elementskit-btn whitespace--normal" href="#">
-<svg fill="none" height="43" viewBox="0 0 43 43" width="43" xmlns="http://www.w3.org/2000/svg"><path d="M28.5396 17.1469L13.3776 32.3089L10.8867 29.818L26.047 14.656H12.6853V11.1328H32.0628V30.5104H28.5396V17.1469Z" fill="currentColor"></path></svg> </a>
-</div>
-</div>
-</div>
-</div>
-</div> </div>
-</div>
-<div className="elementor-element elementor-element-4f2c651 elementor-widget__width-initial agency-social-item ekit-equal-height-disable elementor-invisible elementor-widget elementor-widget-elementskit-icon-box" data-e-type="widget" data-element_type="widget" data-id="4f2c651" data-settings="{&quot;_animation&quot;:&quot;fadeInUp&quot;,&quot;_animation_delay&quot;:100,&quot;ekit_we_effect_on&quot;:&quot;none&quot;}" data-widget_type="elementskit-icon-box.default">
-<div className="elementor-widget-container">
-<div className="ekit-wid-con"> {/* link opening */}
-{/* end link opening */}
-<div className="elementskit-infobox text-left text-left icon-top-align elementor-animation-">
-<div className="elementskit-box-header elementor-animation-">
-<div className="elementskit-info-box-icon">
-<svg aria-hidden="true" className="elementkit-infobox-icon e-font-icon-svg e-fab-linkedin-in" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg"><path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path></svg>
-</div>
-</div>
-<div className="box-body">
-<h3 className="elementskit-info-box-title">
-                    Join Us On Linked In                </h3>
-<p>user.artistic_official</p>
-<div className="box-footer disable_hover_button">
-<div className="btn-wraper">
-<a className="elementskit-btn whitespace--normal" href="#">
-<svg fill="none" height="43" viewBox="0 0 43 43" width="43" xmlns="http://www.w3.org/2000/svg"><path d="M28.5396 17.1469L13.3776 32.3089L10.8867 29.818L26.047 14.656H12.6853V11.1328H32.0628V30.5104H28.5396V17.1469Z" fill="currentColor"></path></svg> </a>
+<div className="brandexa-social-grid">
+{[
+  {
+    name: "LinkedIn",
+    title: "Join Us On LinkedIn",
+    handle: "@brandexagrowth",
+    href: "https://www.linkedin.com/company/brandexagrowth/?viewAsMember=true",
+    icon: (
+      <svg aria-hidden="true" viewBox="0 0 448 512"><path d="M100.28 448H7.4V148.9h92.88V448zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path></svg>
+    ),
+  },
+  {
+    name: "Facebook",
+    title: "Follow On Facebook",
+    handle: "@brandexagrowth",
+    href: "https://www.facebook.com/brandexagrowth/",
+    icon: (
+      <svg aria-hidden="true" viewBox="0 0 320 512"><path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path></svg>
+    ),
+  },
+  {
+    name: "YouTube",
+    title: "Follow On YouTube",
+    handle: "@brandexagrowth",
+    href: "https://www.youtube.com/@brandexagrowth",
+    icon: (
+      <svg aria-hidden="true" viewBox="0 0 576 512"><path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597C14.999 166.945 15 256 15 256s-.001 89.055 11.345 131.917c6.281 23.65 24.787 42.276 48.284 48.597C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.322 42.003-24.947 48.284-48.597C561.001 345.055 561 256 561 256s.001-89.055-11.345-131.917zM232 334V178l142 78-142 78z"></path></svg>
+    ),
+  },
+  {
+    name: "Instagram",
+    title: "Follow On Instagram",
+    handle: "@brandexagrowth",
+    href: "https://www.instagram.com/brandexagrowth/",
+    icon: (
+      <svg aria-hidden="true" viewBox="0 0 448 512"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path></svg>
+    ),
+  },
+].map((social) => (
+<a className="brandexa-social-card" href={social.href} key={social.name} rel="noopener noreferrer" target="_blank">
+<span className="brandexa-social-icon">{social.icon}</span>
+<span className="brandexa-social-text">
+<span>{social.title}</span>
+<strong>{social.handle}</strong>
+</span>
+<span className="brandexa-social-arrow" aria-hidden="true">
+<svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M7 17L17 7M17 7H8M17 7V16" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></path></svg>
+</span>
+</a>
+))}
 </div>
 </div>
-</div>
-</div>
-</div> </div>
-</div>
-<div className="elementor-element elementor-element-09e1e1b elementor-widget__width-initial agency-social-item ekit-equal-height-disable elementor-invisible elementor-widget elementor-widget-elementskit-icon-box" data-e-type="widget" data-element_type="widget" data-id="09e1e1b" data-settings="{&quot;_animation&quot;:&quot;fadeInUp&quot;,&quot;_animation_delay&quot;:200,&quot;ekit_we_effect_on&quot;:&quot;none&quot;}" data-widget_type="elementskit-icon-box.default">
-<div className="elementor-widget-container">
-<div className="ekit-wid-con"> {/* link opening */}
-{/* end link opening */}
-<div className="elementskit-infobox text-left text-left icon-top-align elementor-animation-">
-<div className="elementskit-box-header elementor-animation-">
-<div className="elementskit-info-box-icon">
-<i aria-hidden="true" className="elementkit-infobox-icon icon icon-instagram-1"></i>
-</div>
-</div>
-<div className="box-body">
-<h3 className="elementskit-info-box-title">
-                    Follow On Instagram                </h3>
-<p>@artistic_insta_official</p>
-<div className="box-footer disable_hover_button">
-<div className="btn-wraper">
-<a className="elementskit-btn whitespace--normal" href="#">
-<svg fill="none" height="43" viewBox="0 0 43 43" width="43" xmlns="http://www.w3.org/2000/svg"><path d="M28.5396 17.1469L13.3776 32.3089L10.8867 29.818L26.047 14.656H12.6853V11.1328H32.0628V30.5104H28.5396V17.1469Z" fill="currentColor"></path></svg> </a>
-</div>
-</div>
-</div>
-</div>
-</div> </div>
-</div>
-</div>
-</div>
-</div>
+</section>
 <div className="elementor-element elementor-element-419375d how-it-work e-flex e-con-boxed e-con e-parent" data-e-type="container" data-element_type="container" data-id="419375d">
 <div className="e-con-inner">
 <div className="elementor-element elementor-element-ebecc5c e-con-full e-flex e-con e-child" data-e-type="container" data-element_type="container" data-id="ebecc5c">
@@ -1084,5 +1050,3 @@ export default function Home() {
 </div>
   );
 }
-
-
